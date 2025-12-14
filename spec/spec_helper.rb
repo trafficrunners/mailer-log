77054b95fa88e3fcd4b6b4902f5e63c948502424
sk <sk@httplab.ru>
2025-12-14 07:46:49 +0400

    Update spec_helper path for standalone gem location

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-# Load the main app's rails_helper to get full test environment
-require_relative '../../../spec/rails_helper'
+# Load the host app's rails_helper to get full test environment
+# Adjust path as needed for your host app location
+require_relative '../../gmbmanager/spec/rails_helper'
 
 # Load engine factories into FactoryBot
 Dir[MailerLog::Engine.root.join('spec/factories/**/*.rb')].each { |f| require f }
